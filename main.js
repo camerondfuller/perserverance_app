bae6e6b884baeb974d13490c36ab6e3d91a94f33
Cameron Fuller <franksjacket@yahoo.com>
2016-02-16 18:16:35 -0800

    Add empty else if statement to main.js.

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
 $(function() {
+   $('button').on('click', function() {
+      var results = math.random();
+      if(results === 0) {
 
+      } else if (results === 0.05) {
+
+      } else if (results === 0.1) {
+
+      }else if (results === 0.15) {
+
+      }else if (results === 0.2) {
+
+      }else if (results === 0.25) {
+
+      }else if (results === 0.3) {
+
+      }else if (results === 0.35) {
+
+      }else if (results === 0.4) {
+
+      }else if (results === 0.45) {
+
+      }else if (results === 0.5) {
+
+      }else if (results === 0.55) {
+
+      }else if (results === 0.6) {
+
+      }else if (results === 0.65) {
+
+      }else if (results === 0.7) {
+
+      }else if (results === 0.75) {
+
+      }else if (results === 0.8) {
+
+      }else if (results === 0.85) {
+
+      }else if (results === 0.9) {
+
+      }else if (results === 0.95) {
+
+      } else {
+
+      }
+   });
 });
